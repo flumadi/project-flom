6859c8110286917c1871a78e913ad080af0c9b3b
FridahMathias <170721231+flumadi@users.noreply.github.com>
2024-10-06 12:11:51 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Contact Form</h2>
+
+<form action="submit_form.php" method="post">
+  <label for="name">Name:</label><br>
+  <input type="text" id="name" name="name"><br>
+  <label for="phone">Phone Number:</label><br>
+  <input type="tel" id="phone" name="phone"><br>
+  <label for="email">Email:</label><br>
+  <input type="email" id="email" name="email"><br>
+  <label for="message">Message:</label><br>
+  <textarea id="message" name="message"></textarea><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
---
+++ subscribe.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form>
+<form action="submit_form.php" method="post">
+  <label for="email">Email:</label><br>
+  <input type="text" id="email" name="email" placeholder="Enter your email"><br>
+    <button>Subscribe</button>
+</form>
+
+
+</body>
+</html>
